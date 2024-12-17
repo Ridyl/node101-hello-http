696b550c2d2b41f5cd9c2b2597304448d3153ce9
Ridyl <joey.dalatri214@gmail.com>
2024-12-17 16:55:29 -0500

    server responds to GET with status code 200, logs each request at console, indicates which port it is listening on and passes all tests.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// write your code here
+const http = require('http');
+
+const server = http.createServer(function(req, res) {
+    console.log(req.method + req.url);
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
+    res.end("Hello World");
+});
+
+server.listen(8080, 'localhost', null, function(){
+    console.log('Server host is listening on localhost:8080');
+});
